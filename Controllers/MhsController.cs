ae977c70e1dcd28ebbb7798fdd134bfca5888da6
Hasannp0611 <hasannp0611@gmail.com>
2024-04-29 00:01:19 +0700

    Menambahkan API Mahasiswa

---
+++ Controllers/MhsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace tpmodul10_1302220031.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MhsController : ControllerBase
+    {
+        private static List<Mahasiswa> listMhs = new List<Mahasiswa>
+        {
+            new Mahasiswa("Hasan Nurrahman Pane", "1302220031"),
+            new Mahasiswa("Marjauza Naswansyah", "1302220009"),
+            new Mahasiswa("Nizar Rasyiid", "1302220037"),
+            new Mahasiswa("Matthew Theodore Lempas", "1302223116")
+        };
+
+        [HttpGet]                                               //Untuk menampilkan list mahasiswa
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return listMhs;
+        }
+
+        [HttpPost]                                              //Untuk menambahkan objek baru
+        public void Post([FromBody] Mahasiswa mahasiswa)
+        {
+            listMhs.Add(mahasiswa);
+        }
+
+        [HttpGet("{id}")]                                       //Untuk menampilkan list berdasar id
+        public Mahasiswa Get(int id)
+        {
+            return listMhs[id];
+        }
+
+        [HttpDelete("{id}")]                                    //Untuk menghapus objek yang ada
+        public void Delete(int id)
+        {
+            listMhs.RemoveAt(id);
+        }
+    }
+}
+
---
+++ Mahasiswa.cs
@@ -0,0 +1,15 @@
+﻿namespace tpmodul10_1302220031
+{
+	public class Mahasiswa
+	{
+		public String Nama { get; set; }		//saya mencoba menggunakan private tapi gagal
+		public String Nim { get; set; }
+
+        public Mahasiswa(string Nama, string Nim)
+		{
+			this.Nama = Nama;
+            this.Nim = Nim;
+		}
+	}
+}
+
